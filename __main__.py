dab47d3c7529b002bfeb58ab8a039cfbd52b9550
abidibo <abidibo@gmail.com>
2018-11-21 11:14:56 +0100

    reafactoring and output

--- __main__.py
+++ __main__.py
@@ -7,15 +7,17 @@ from feedchecker import Checker
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", dest="input",
                     help="input path to check and filter")
-parser.add_argument("-o", "--output", dest="ouput",
+parser.add_argument("-o", "--output", dest="output",
                     help="output path")
 
 args = parser.parse_args()
 
 if not args.input:
     sys.exit('Specify an input file, run with -h option for help')
+if not args.output:
+    sys.exit('Specify an output file, run with -h option for help')
 if not os.path.isfile(args.input):
     sys.exit('Cannot find the input file, dude')
 
-checker = Checker(args.input)
+checker = Checker(args.input, args.output)
 checker.run()
