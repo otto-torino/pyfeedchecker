6d59a7acc767f757d326e49f402e5cb999203e2a
abidibo <abidibo@gmail.com>
2018-11-21 11:14:56 +0100

    reafactoring and ouput

--- feedchecker.py
+++ feedchecker.py
@@ -1,24 +1,32 @@
 import json
+import logging
+
 import grequests
 
+logging.basicConfig(filename='.pyfeedchecker.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 class Checker():
-    def __init__(self, json_path):
-        self.hjson_path = json_path
-        self.corrected_hjson = []
+    def __init__(self, input, output):
+        self.json_path = input
+        self.output_path = output
+        self.corrected_json = []
 
     def hook_factory(self, site):
         def response_hook(response, *request_args, **request_kwargs):
-            print('========================================================')
-            print(response.url)
-            print("elapsed time: %.2f" % response.elapsed.total_seconds())
-            print("status code: %d" % response.status_code)
+            logger.info('Checking %s' % response.url)
+            logger.info(
+                'Elapsed Time: %.2f' % response.elapsed.total_seconds())
+            logger.info('Status Code: %d' % response.status_code)
             # keep only 200 <= codes < 300
             # redirects (3XX) causes this hook to be fired again, so are
             # not taken into account
             if (response.status_code < 300):
-                self.corrected_hjson.append(site)
+                self.corrected_json.append(site)
 
-            return response  # or the modified response
+            return response
+
         return response_hook
 
     def run(self):
@@ -27,11 +35,11 @@ class Checker():
         totals = {}
         bad_results = 0
 
-        print(self.hjson_path)
-        with open(self.hjson_path) as f:
+        with open(self.json_path) as f:
             sites = json.load(f)
 
-        rs = (grequests.head(s.get('url'), hooks={'response': [self.hook_factory(s)]})
+        rs = (grequests.head(
+            s.get('url'), hooks={'response': [self.hook_factory(s)]})
               for s in sites.get('base_urls'))
         for r in grequests.imap(rs, size=20):
             total += 1
@@ -42,14 +50,22 @@ class Checker():
             if r.status_code >= 400:
                 bad_results += 1
 
-        print('========================================================')
-        print('Summary')
-        print('========================================================')
-        print('Total requests: %d' % total)
-        print('Bad responses: %d' % bad_results)
+        logger.info('Summary')
+        logger.info('Total Requests: %d' % total)
+        logger.info('Bad Responses: %d' % bad_results)
         for sc in totals:
-            print('Status Code %d: %d' % (sc, totals[sc]))
+            logger.info('Status Code %d: %d' % (sc, totals[sc]))
 
+        self.write_corrected_json()
 
-    def get_corrected_hjson(self):
-        return self.corrected_hjson
+    def write_corrected_json(self):
+        with open(self.output_path, 'w') as outfile:
+            json.dump(
+                self.corrected_json,
+                outfile,
+                sort_keys=True,
+                indent=4,
+                ensure_ascii=False)
+
+    def get_corrected_json(self):
+        return self.corrected_json
