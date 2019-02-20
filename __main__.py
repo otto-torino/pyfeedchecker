d23187e1616eeeec7d72b5431c69ed3664f3ed9b
abidibo <abidibo@gmail.com>
2019-02-20 10:42:14 +0100

    adds timeout feature

--- __main__.py
+++ __main__.py
@@ -1,14 +1,23 @@
-import sys
 import os
+import sys
 from argparse import ArgumentParser
 
 from feedchecker import Checker
 
 parser = ArgumentParser()
-parser.add_argument("-i", "--input", dest="input",
-                    help="input path to check and filter")
-parser.add_argument("-o", "--output", dest="output",
-                    help="output path")
+parser.add_argument(
+    "-i", "--input", dest="input", help="input path to check and filter")
+parser.add_argument("-o", "--output", dest="output", help="output path")
+parser.add_argument(
+    '--timeout',
+    dest='request_timeout',
+    type=int,
+    default=10,
+    help='request timeout seconds')
+parser.add_argument(
+    '--oktimeout',
+    action='store_false',
+    help='considers a timeout response as a good response')
 
 args = parser.parse_args()
 
@@ -19,5 +28,6 @@ if not args.output:
 if not os.path.isfile(args.input):
     sys.exit('Cannot find the input file, dude')
 
-checker = Checker(args.input, args.output)
+checker = Checker(args.input, args.output, args.request_timeout,
+                  args.oktimeout)
 checker.run()
